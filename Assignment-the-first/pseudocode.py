b7a2e395bb601a121a8a205bb8fbc15b48ba1885
PowersPope <apowers4@uoregon.edu>
2021-07-29 16:29:27 -0700

    Added the pseudo code file

---
+++ Assignment-the-first/pseudocode.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+files = R1, R2, R3, R4
+
+index_dict = {48 keys and values} #24 indexes normal and 24 indexes rev comp as keys and their corresponding values 
+
+open R1, R2, R3, R4 as read1, index1, index2, read2: #R1-4 is the illumina read files
+
+    open files 1-52 as output1 to output52: #I will have to write out the 52 output files that are needed to write to.
+
+        read 4 lines: #Read 4 lines in all of the files at once and store that into a dict
+
+        dict = {read1: [4 lines], index1: [4 lines], index2: [4 lines], read2: [4 lines} #store all lines in a dict with a list as a value of the 4 lines
+
+            if index1 in index_dict: #Check to see if index one is in the dictionary of all possible indexes
+
+                if index2 == index_dict[index1]: #check to see if the index2 matches the values of the dict
+                    add index1 + "-" + index2 to header in read1 and read2 #Add the indexes to the header of both reads
+                    
+                    write read1 to read1file and read2 to read2file in bucket of index1_match #Write this to the appropriate files in the correct index bucket
+
+                else: #If they don't match then it indexed hopped
+                    add index1 + "-" + index2 to header in read1 and read2 #Add the headers to the 2 reads
+                    write these into index hopping #Write these to the appropriate files in the index hopping bucket
+
+            else: #If the index is not in the index_dict then it is trash
+                add index1 + "-" + index2 to header in read1 and read2 #Add the indexes to the two reads
+
+                write to trash_bucket #Write these to the appropriate files in the trash bucket
